f0541927be8f1867b9ed999c977e2902fe491989
Olesya <olesyabrazhnikova@ukr.net>
2023-01-23 01:27:26 +0200

    added files

---
+++ app/js/main.js
@@ -0,0 +1,55 @@
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const communitySlider = new Swiper(".block-community__slider", {
+// 		freeMode: true,
+// 		pagination: {
+// 			el: ".swiper-pagination",
+// 			clickable: true,
+// 		},
+// 		slidesPerView: 1,
+// 		spaceBetween: 0,
+// 		breakpoints: {
+// 			567: {
+// 				slidesPerView: 2,
+// 			},
+// 			991: {
+// 				slidesPerView: 3,
+// 			},
+// 			1024: {
+// 				slidesPerView: 4,
+// 			},
+// 		},
+// 	});
+// });
+
+// //ACCORDION
+
+// var acc = document.getElementsByClassName("accordion");
+// var panel = document.getElementsByClassName('panel');
+
+// for (var i = 0; i < acc.length; i++) {
+// 	acc[i].onclick = function () {
+// 		var setClasses = !this.classList.contains('active');
+// 		setClass(acc, 'active', 'remove');
+// 		setClass(panel, 'show', 'remove');
+
+// 		if (setClasses) {
+// 			this.classList.toggle("active");
+// 			this.nextElementSibling.classList.toggle("show");
+// 		}
+// 	}
+// }
+
+// function setClass(els, className, fnName) {
+// 	for (var i = 0; i < els.length; i++) {
+// 		els[i].classList[fnName](className);
+// 	}
+// }
+
+
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
